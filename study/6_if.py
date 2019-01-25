9465eafcb14ee57dea46901e2072ed68439f96c9
BlackBoneStorm <41055339+BlackBoneStorm@users.noreply.github.com>
2019-01-25 13:58:32 +0200

    while

---
+++ study/6_if.py
@@ -0,0 +1,22 @@
+a = 2
+if a == 3:
+    print("hey 2")
+if a > 4:
+    print("hey 3")
+else:
+    print("looser")
+
+q = 10
+w = True if q < 10 else False
+
+a = float(input("Введи число: "))
+if a < 0:
+    print("Neg")
+elif a == 0:
+    print("Zero")
+elif a == 1:
+    print("One")
+else:
+    print("Pos")
+
+
---
+++ study/7_while.py
@@ -0,0 +1,15 @@
+a = 0
+while a >= 0:
+    if a == 3:
+        break
+    a += 1
+    print("A")
+
+b = -1
+while b < 10:
+    b += 1
+    if b >= 7:
+        continue
+    print("B")
+
+
